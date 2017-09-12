e3a6c2f740d5b2bc9425d2fa3e40884cc51bbde7
Joel Huggett <jhuggett@fnslabs.com>
2017-09-12 12:06:24 -0300

    worldMap now has landMasses where it stores landMass info

--- Viewer/RpgFrameworksViewer/MapGen_WorldGen.swift
+++ Viewer/RpgFrameworksViewer/MapGen_WorldGen.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2017 Joel Huggett. All rights reserved.
 //
 
+// earth's diameter is ~12000 km
+// therefore 12000 / 200 = 60
+// thus each location is 60 km by 60 km
+
 import Foundation
 
 typealias dimensions = (height: Int, width: Int)
@@ -69,6 +73,12 @@ func getDirection(point: inout coor, _ distance: Int)-> coor {
     }
 }
 
+struct landMass {
+    var name: randName = getNewID()
+    var coors = [coor]()
+    
+}
+
 
 struct location {
     var continentID: randName?
@@ -77,61 +87,60 @@ struct location {
     var lType: String {get{return self.landClass.name}}
 }
 
-struct sector {
+struct worldMap {
+    var landMasses = [landMass]()
     var gridSize: dimensions
     var locations = [Int: [Int: location]]()
     var owner: String? = nil
     var numOfCon: Int = 0
     var numOfIsl: Int = 0
-    
     init(gridSize: dimensions) {
     
         self.gridSize = gridSize
-        for i in 0...self.gridSize.height {self.locations[i] = [:]; for t in 0...self.gridSize.width {self.locations[i]![t] = location()}}
+        print("Creating Blank Map - \(gridSize)")
+        for i in 0...self.gridSize.height {self.locations[i] = [:]; for t in 0...self.gridSize.width {self.locations[i]![t] = location(); print("Blank Map - \(gridSize) {y: \(i), x: \(t)}")}}
+        print("Finished Blank Map")
     }
     var readable: String {get{var readable = ""; for i in 0...self.gridSize.height {readable += "\n"; for t in 0...self.gridSize.width {readable += self.locations[i]![t]!.symbol}}; return readable}}
-    
     mutating func genContinent(_ cSize: Int, _ size: Int) {
-        for p in 1...25 {
+        for p in 1...50 {
             let randPoint: coor = (returnRandNumInRange(0, self.gridSize.width)-10, returnRandNumInRange(0, self.gridSize.height)-10)
             if scan(size, randPoint).filter({self.locations[$0.y] == nil || self.locations[$0.y]![$0.x] == nil || self.locations[$0.y]![$0.x]!.lType != "water"}).count > 0 {
-                print("Scan Attempt #\(p)/25")
+                print("Scan Attempt #\(p)/50")
                 continue
             } else {
-                let cID = getNewID()
-                print("<NEW CONTINENT> <\(cID.readable)> START POINT: \(randPoint)")
+                var newLandMass = landMass()
+                print("<NEW CONTINENT> <\(newLandMass.name.readable)> START POINT: \(randPoint)")
                 let startPoint = randPoint
                 var point = startPoint
-                self.locations[point.y]![point.x]!.landClass = ("land", "\(cID.readable.first!) ")
-                self.locations[point.y]![point.x]!.continentID = cID
-
+                if self.locations[point.y]![point.x]!.landClass.name == "water" {newLandMass.coors.append(point)}
+                self.locations[point.y]![point.x]!.landClass = ("land", "\(newLandMass.name.readable.first!) ")
+                self.locations[point.y]![point.x]!.continentID = newLandMass.name
                 for p in 1...cSize {
-                    let sScan = scan(5, point).filter({$0.x < 0 || $0.x > self.gridSize.width || $0.y < 0 || $0.y > self.gridSize.height || self.locations[$0.y]![$0.x]!.continentID != nil && self.locations[$0.y]![$0.x]!.continentID!.readable != cID.readable})
+                    let sScan = scan(5, point).filter({$0.x < 0 || $0.x > self.gridSize.width || $0.y < 0 || $0.y > self.gridSize.height || self.locations[$0.y]![$0.x]!.continentID != nil && self.locations[$0.y]![$0.x]!.continentID!.readable != newLandMass.name.readable})
                     if sScan.count > 0 {
-                        print("\(cID.readable)-DRAW SCAN-OUT OF BOUNDS-LOG: last point: \(point) {MOVE #\(p)/\(cSize)};\nfailing points: \(sScan)")
+                        print("\(newLandMass.name.readable)-DRAW SCAN-OUT OF BOUNDS-LOG: last point: \(point) {MOVE #\(p)/\(cSize)};\nfailing points: \(sScan)")
                         point = startPoint
                     } else {
                         point = getDirection(point: &point, 2)
                         let scanedPoint = scan(1, point)
-                        print("\(cID.readable)-DRAW SUCCESS-POINTS: \(scanedPoint) {MOVE #\(p)/\(cSize)}")
-                        scanedPoint.map({self.locations[$0.y]![$0.x]!.landClass = ("land", "\(cID.readable.first!) "); self.locations[$0.y]![$0.x]!.continentID = cID})
-                        ring(2, point).map({if returnRandomBool(1, 2) { self.locations[$0.y]![$0.x]!.landClass = ("land", "\(cID.readable.first!) "); self.locations[$0.y]![$0.x]!.continentID = cID}})
-                        
-                    }
+                        print("\(newLandMass.name.readable)-DRAW SUCCESS-POINTS: \(scanedPoint) {MOVE #\(p)/\(cSize)}")
+                        for v in scanedPoint {if self.locations[v.y]![v.x]!.landClass.name == "water" {newLandMass.coors.append(v)}; self.locations[v.y]![v.x]!.landClass = ("land", "L "); self.locations[v.y]![v.x]!.continentID = newLandMass.name}
+                        for v in ring(2, point) {if self.locations[v.y]![v.x]!.landClass.name == "water" {newLandMass.coors.append(v)};if returnRandomBool(1, 2) { self.locations[v.y]![v.x]!.landClass = ("land", "L "); self.locations[v.y]![v.x]!.continentID = newLandMass.name}}}
                 }
-                print("|\(cID.readable) - Complete|")
+                self.landMasses.append(newLandMass)
+                print("|\(newLandMass.name.readable) - Complete|")
                 break
             }
         }
     }
+    func landMassDescription() {
+        print("LandMasses")
+        for i in self.landMasses {
+            print("\(i.name.readable): \(i.coors.count) * 60 kilometers squared = \(i.coors.count * 60) km squared")
+        }
+    }
 }
 
-
-struct worldMap {
-    var gridSize: dimensions
-    var sectors: [Int: [Int: sector]]
-    
-    
-}
 
 
